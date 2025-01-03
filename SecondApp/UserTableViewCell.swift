ae5cd43781786c3b00c5feda9ef7c4c00edebdbf
been <leebin0603@gmail.com>
2025-01-03 16:33:03 +0900

    테이블뷰컨트롤러 커스텀셀 구성

---
+++ SecondApp/UserTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  UserTableViewCell.swift
+//  SecondApp
+//
+//  Created by 이빈 on 1/3/25.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var messageLabel: UILabel!
+    
+    
+    
+}
--- SecondApp/UserTableViewController.swift
+++ SecondApp/UserTableViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class UserTableViewController: UITableViewController {
 
+    let name = ["고래밥", "칙촉", "카스타드"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return name.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath) as! UserTableViewCell
+        
+        cell.profileImageView.backgroundColor = .brown
+        cell.nameLabel.text = name[indexPath.row]
+        cell.messageLabel.text = "상태 메시지"
+        
+        cell.nameLabel.font = .boldSystemFont(ofSize: 15)
+        cell.messageLabel.font = .systemFont(ofSize: 13)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
 
     
